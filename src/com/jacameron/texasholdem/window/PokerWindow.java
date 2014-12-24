f6cb6a585166ef718a1120985c4d5cbc4d2d6846
Jonathan Cameron <jacprogrammer@hotmail.com>
2014-12-23 20:53:56 -0800

    Modular spacing v1 complete

--- src/com/jacameron/texasholdem/window/PokerWindow.java
+++ src/com/jacameron/texasholdem/window/PokerWindow.java
@@ -97,19 +97,21 @@ public class PokerWindow extends JPanel implements ActionListener
 		
 	}
 	
+	//Buttons and settings for the card stack
 	public void cardStack(int stackNumber)
 	{
-		final int SPACING = 15; 		//y coordinate spacing between radio buttons
-		int x = 0;
-		int y = 0;
-		int length = 60 * stackNumber;
-		int width  = 15 * stackNumber;
+		//These settings start the stack on the bottom of the
+		//window. The code allows for multiple stacks, and makes
+		//the spacing adjustments for each stack
 		
-		//track what stack number it is for sizing
-		int currentStackNumber = stackNumber;
+		final int SPACING = 15; 		//y coordinate spacing between radio buttons
+		int x = 20;
+		int y = 180;
+		int length = 60;
+		int width  = 15;
 		
 		//Check what stack number it is, and size accordingly
-		if(currentStackNumber == 0)
+		if(stackNumber == 1 || stackNumber == 0)
 		{
 			
 		}else
@@ -119,87 +121,74 @@ public class PokerWindow extends JPanel implements ActionListener
 		
 		//Deck of cards(String)
         Cards cards = new Cards();        
+
+        //Group the radio buttons.
+        ButtonGroup group = new ButtonGroup();
+        setLayout(null);
+
+        //Put the radio buttons in a column in a panel.
+        JPanel radioPanel = new JPanel();
+        radioPanel.setBounds(x, y, length, width);
+        radioPanel.setLayout(null);
+
+        add(radioPanel);
+//      add(picture, BorderLayout.CENTER);
+        setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
         
         //Create the radio buttons.
         //Ace
         JRadioButton ace = new JRadioButton(cards.getCardAce());
         ace.setBounds(x, y, length, width);
+        add(ace);
         ace.setMnemonic(KeyEvent.VK_1);
         ace.setActionCommand(cards.getCardAce());
         ace.setSelected(true);
-
+        group.add(ace);
+        ace.addActionListener(this);
+        
         //King
         JRadioButton king = new JRadioButton(cards.getCardKing());
-        king.setBounds(x, (1 * SPACING), length, width);
+        king.setBounds(x,(y + (SPACING * 1)), length, width);
+        add(king);
         king.setMnemonic(KeyEvent.VK_2);
         king.setActionCommand(cards.getCardKing());
-
+        group.add(king);
+        king.addActionListener(this);
+        
         //Queen
         JRadioButton queen = new JRadioButton(cards.getCardQueen());
-        queen.setBounds(x, (2 * SPACING), length, width);
+        queen.setBounds(x, (y + (SPACING * 2)), length, width);
+        add(queen);
         queen.setMnemonic(KeyEvent.VK_3);
         queen.setActionCommand(cards.getCardQueen());
-
+        group.add(queen);
+        queen.addActionListener(this);
+        
         //Jack
         JRadioButton jack = new JRadioButton(cards.getCardJack());
-        jack.setBounds(x, (3 * SPACING), length, width);
+        jack.setBounds(x, (y + (SPACING * 3)), length, width);
+        add(jack);
         jack.setMnemonic(KeyEvent.VK_4);
         jack.setActionCommand(cards.getCardJack());
-
+        group.add(jack);
+        jack.addActionListener(this);
+        
         //Ten
         JRadioButton ten = new JRadioButton(cards.getCardTen());
-        ten.setBounds(x, (4* SPACING), length, width);
+        ten.setBounds(x, (y + (SPACING * 4)), length, width);
+        add(ten);
         ten.setMnemonic(KeyEvent.VK_5);
         ten.setActionCommand(cards.getCardTen());
+        group.add(ten);
+        ten.addActionListener(this);
         
         //Nine
         JRadioButton nine = new JRadioButton(cards.getCardNine());
-        nine.setBounds(x, (5 * SPACING), length, width);
+        nine.setBounds(x, (y + (SPACING * 5)), length, width);
+        add(nine);
         nine.setMnemonic(KeyEvent.VK_6);
         nine.setActionCommand(cards.getCardNine());
-
-        //Group the radio buttons.
-        ButtonGroup group = new ButtonGroup();
-        group.add(ace);
-        group.add(king);
-        group.add(queen);
-        group.add(jack);
-        group.add(ten);
         group.add(nine);
-
-        //Register a listener for the radio buttons.
-        ace.addActionListener(this);
-        king.addActionListener(this);
-        queen.addActionListener(this);
-        jack.addActionListener(this);
-        ten.addActionListener(this);
         nine.addActionListener(this);
-        setLayout(null);
-
-//        //Set up the picture label.
-//        picture = new JLabel(createImageIcon("images/"
-//                                             + birdString
-//                                             + ".gif"));
-//
-//        //The preferred size is hard-coded to be the width of the
-//        //widest image and the height of the tallest image.
-//        //A real program would compute this.
-//        picture.setPreferredSize(new Dimension(177, 122));
-
-
-        //Put the radio buttons in a column in a panel.
-        JPanel radioPanel = new JPanel();
-        radioPanel.setBounds(x, (1 * SPACING), length, width);
-        radioPanel.setLayout(null);
-        radioPanel.add(ace);
-        radioPanel.add(king);
-        radioPanel.add(queen);
-        radioPanel.add(jack);
-        radioPanel.add(ten);
-        radioPanel.add(nine);
-
-        add(radioPanel);
-//        add(picture, BorderLayout.CENTER);
-        setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 	}
 }
